1f0839df2d9eb9a31f017bdfec4e14aef96972a0
MamyAndry <119005054+MamyAndry@users.noreply.github.com>
2024-02-01 09:05:17 +0300

    alter functions

---
+++ kombarika/com/district/test/repository/RegionRepository.java
@@ -0,0 +1,19 @@
+package com.district.test.repository;
+
+
+import com.district.test.entity.Region;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+
+
+public interface RegionRepository extends CrudRepository<Region, Integer> {
+
+
+
+
+
+
+
+
+
+}
