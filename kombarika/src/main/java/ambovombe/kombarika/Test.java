f897fe0df81167af59bd44881a4e2d0725a11dac
01KingMaker <fabrakoto0987@gmail.com>
2024-03-20 08:19:01 +0300

    vue js

--- kombarika/src/main/java/ambovombe/kombarika/Test.java
+++ kombarika/src/main/java/ambovombe/kombarika/Test.java
@@ -4,6 +4,8 @@ package ambovombe.kombarika;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 
+import java.sql.SQLException;
+
 import ambovombe.kombarika.database.DbProperties;
 import ambovombe.kombarika.generator.CodeGenerator;
 import ambovombe.kombarika.generator.service.DbService;
@@ -21,7 +23,8 @@ public class Test {
     public static void main(String[] args) throws Exception {
         CodeGenerator codeGenerator = new CodeGenerator();  
         String path = "/home/fabien/Document/java"; // class path
-        String viewPath = "/home/fabien/Documents/vue";
+        path = "/home/fabien/Documents/my-app(1)/my-app/src/main/java/io/bootify/my_app/generatedrest2/";
+        String viewPath = "/home/fabien/Documents/GitHub/meteo-dashboard/src/components";
         
         // String path = "/home/mamisoa/ITU/L3/Mr_Naina/FRAMEWORK/testkarana/src/main/java";
         // String viewPath = "./";
@@ -30,8 +33,8 @@ public class Test {
         // String viewPath = "/home/mamisoa/ITU/L3";
 
 
-        String framework = "java:karana";
-        String packageName = "com.district.test";
+        String framework = "java:spring-boot";
+        String packageName = "io.bootify.my_app.generatedrest";
         String entity = "entity";
         String controller = "controller";
         String repository = "repository";
---
+++ src/main/java/io/bootify/my_app/genereratedrest/io/bootify/my_app/generatedrest/controller/DistrictModel.java
@@ -0,0 +1,53 @@
+package io.bootify.my_app.generatedrest.controller;
+
+import io.bootify.my_app.generatedrest.entity.District;
+import com.karana.etu2060.framework.annotation.*;
+import com.karana.etu2060.framework.annotation.property.*;
+import com.dao.database.GenericDao;
+
+@RequestMapping(path = "/district")
+public class DistrictModel {
+	private District entity = new District();
+
+	@Url(method = HttpMethod.POST)
+	@Json
+	public Object save(@RequestBody District district){
+	 	try{
+			GenericDao.save(null, district);
+			return "saved successfully";
+		}catch(Exception e){
+			return "Bad request";
+		}
+	}
+	@Url(method = HttpMethod.PUT)
+	@Json
+	public Object update(@RequestBody District district){
+	 	try{
+			GenericDao.update(null, district);
+			return "updated successfully";
+		}catch(Exception e){
+			return "Bad request";
+		}
+	}
+	@Url(method = HttpMethod.DELETE)
+	@Json
+	public Object delete(@RequestBody District district){
+	 	try{
+			GenericDao.delete(null, district);
+			return "deleted successfully";
+		}catch(Exception e){
+			return "Bad request";
+		}
+	}
+	@Url(method = HttpMethod.GET)
+	@Json
+	public Object findAll(){
+	 	try{
+			return GenericDao.findAll(null, entity);
+		}catch(Exception e){
+			return "Bad request";
+		}
+	}
+
+
+}
