16eb17d812af33d542ebfa729753950d4744d0d6
rakharrs <rodyox.yoann@gmail.com>
2024-03-20 06:39:29 +0300

    test

--- kombarika/src/main/java/ambovombe/kombarika/Test.java
+++ kombarika/src/main/java/ambovombe/kombarika/Test.java
@@ -21,18 +21,18 @@ public class Test {
     public static void main(String[] args) throws Exception {
         CodeGenerator codeGenerator = new CodeGenerator();  
         // String path = "/home/mamisoa/ITU/L3/Mr_Naina/TEST/test/src/main/java";
-        String path = "./";
+        String path = "/Users/rakharrs/S5/Naina/testMerana/demo/src/main/java";
         // String viewPath = "/home/mamisoa/ITU/L3/Mr_Naina/REACT/crud/src/components";
         
         // String path = "/home/mamisoa/ITU/L3/Mr_Naina/FRAMEWORK/testkarana/src/main/java";
-        String viewPath = "./";
+        String viewPath = "/Users/rakharrs/S5/Naina/test/view";
 
         // String path = "/home/mamisoa/ITU/L3";
         // String viewPath = "/home/mamisoa/ITU/L3";
 
 
-        String framework = "csharp:dotnet";
-        String packageName = "com.ditrict";
+        String framework = "java:merana";
+        String packageName = "com.example.demo";
         String entity = "entity";
         String controller = "controller";
         String repository = "repository";
